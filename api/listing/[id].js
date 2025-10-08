2c3c0f9fcd6df2b8b3342397db2f1ae06f6b2d90
Michał Szeniak <michal.szeniak@gmail.com>
2025-10-08 14:42:49 +0200

    save point

--- api/listing/[id].js
+++ api/listing/[id].js
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
 const supabase = createClient(
-    supabaseUrl,
-    supabaseAnonKey
+  process.env.VITE_SUPABASE_URL,           
+  process.env.SUPABASE_SERVICE_ROLE_KEY    
 );
 
 export default async function handler(req, res) {
