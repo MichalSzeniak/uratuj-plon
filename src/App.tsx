52087e06cf53a8352ddd7bc633b84043560628b9
Michał Szeniak <michal.szeniak@gmail.com>
2025-10-09 07:44:05 +0200

    feat: save point

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
         <Router>
           <AuthProvider>
             <div className="min-h-screen ">
-              <AppRoutes />
               <Toaster />
+              <AppRoutes />
             </div>
           </AuthProvider>
         </Router>
