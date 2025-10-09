b4fd3871fab23e40052c78e04c005845116d8901
Michał Szeniak <michal.szeniak@gmail.com>
2025-10-09 08:54:57 +0200

    meta test

---
+++ api/og.js
@@ -0,0 +1,45 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    
+    const title = searchParams.get('title') || 'Default Title';
+    const description = searchParams.get('description') || 'Default Description';
+    const path = searchParams.get('path') || '/';
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#fff',
+            fontSize: 32,
+            fontWeight: 600,
+          }}
+        >
+          <div>{title}</div>
+          <div style={{ fontSize: 24, marginTop: 20 }}>{description}</div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (e) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
